baefa9fe87da4e95f408cafae2dcd5206e110ff8
lucasnety <lucas.nunes@sou.unifeob.edu.br>
2025-10-25 10:54:53 -0300

    Revert "remoçao de descriçao item"
    This reverts commit d44fe4b22c43368689a27cae0e54e60d728aee70.

--- inventario/slot.gd
+++ inventario/slot.gd
@@ -5,7 +5,7 @@ extends PanelContainer
 func set_slot_data(slot_data: SlotData) -> void:
 	var item_data = slot_data.item_data
 	texture_rect.texture = item_data.texture
-	tooltip_text = "%s\n%s" % [item_data.nome]
+	tooltip_text = "%s\n%s" % [item_data.nome, item_data.descricao]
 
 	if slot_data.quantidade > 1:
 		quantity_label.text = "x%s" % slot_data.quantidade
