1b780a673febfb1c06ef8074032cfbdb89a97096
Ale171104 <valero7rinke@gmail.com>
2025-11-27 15:59:11 -0300

    correçao de bugs musica

--- Cenas/musica_mundo.gd
+++ Cenas/musica_mundo.gd
@@ -1,10 +1,24 @@
 extends AudioStreamPlayer3D
 
 func _ready():
+	
+	attenuation_model = AudioStreamPlayer3D.ATTENUATION_DISABLED
+	max_distance = 200
+
+	
+	emission_angle_degrees = 360
+	emission_angle_filter_attenuation_db = 0
+
+	
+	doppler_tracking = AudioStreamPlayer3D.DOPPLER_TRACKING_DISABLED
+
+	
+	panning_strength = 0.0
+
 	if stream:
 		stream.loop = true
-	volume_db = -30.0     # começa baixo
+
+	volume_db = -30.0
 	play()
-	
-	# efeito de aumento suave da música
-	create_tween().tween_property(self, "volume_db", -4.0, 4.0)
+
+	create_tween().tween_property(self, "volume_db", -14.0, 4.0)
